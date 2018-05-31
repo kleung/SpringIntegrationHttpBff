01e1c64b9564a2144095c90c47a91a972c1a72b0
Kiu Kwan Leung <ken_kk_leung@manulifeam.com>
2018-05-31 09:45:01 +0800

    code refactoring

--- TestSplitAggregate/src/main/java/com/test/splitAggregate/TestSplitAggregateApplication.java
+++ TestSplitAggregate/src/main/java/com/test/splitAggregate/TestSplitAggregateApplication.java
@@ -2,10 +2,12 @@ package com.test.splitAggregate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(value= {"classpath:/integration/test-split-aggregate.xml"})
+@ComponentScan(basePackages= {"com.test.splitAggregate"})
 public class TestSplitAggregateApplication {
 
 	public static void main(String[] args) {
